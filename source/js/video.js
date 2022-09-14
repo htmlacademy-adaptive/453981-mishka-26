857551e256170e6a3d94a3bc833992cc38450ba9
Victoria Yakovleva <vikyss@mail.ru>
2022-09-15 01:29:32 +0300

    fix: fix project by criteries

--- source/js/video.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const imageContainer = document.querySelector('.production__image-container')
-  const videoContainer = document.querySelector('.production__video-container')
-
-  imageContainer.addEventListener('click', () => {
-    videoContainer.classList.remove('visually-hidden')
-  })
-})
